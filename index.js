635088e887d7cd861c12a48e2157ec03d26cd291
xezian <xezian@icloud.com>
2019-01-09 22:40:15 -0700

    extremely simple static site server established

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+const staticPath = path.join(__dirname, '/public');
+app.use(express.static(staticPath));
+app.get('/', (req, res) => {
+    res.send('index')
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
