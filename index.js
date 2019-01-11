d129d5f49fdac251d2767d1cc0abaec3f771af49
xezian <xezian@icloud.com>
2019-01-10 23:11:26 -0700

    loaded far too much externals into this simple project to include a tiny code editor

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-const staticPath = path.join(__dirname, '/public');
+const staticPath = path.join(__dirname, "/public");
 app.use(express.static(staticPath));
-app.get('/', (req, res) => {
-    res.send('index')
-})
+app.get("/", (req, res) => {
+  res.send("index");
+});
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`));
--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,66 +1,13 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext('2d');
-var cw = canvas.width;
-var ch = canvas.height;
-
-var canvases = document.getElementsByClassName("myCanvas");
-for (var i = 0; i < canvases.length; i++) {
-	canvases[i].onclick = function(e) {
-		var oldcan = document.getElementById("myCanvas")
-		oldcan.removeAttribute("id");
-		oldcan.setAttribute("style","border-radius:none;");
-		e.target.setAttribute("id", "myCanvas");
-		canvas = document.getElementById("myCanvas");
-		canvas.setAttribute("style","border-radius:10px;");
-		ctx = canvas.getContext('2d');
-		cw = canvas.width;
-		ch = canvas.height;
-	};
+window.onload = function() {
+  const myFunction = document.getElementById("function");
+  require([
+    "../codemirror/lib/codemirror",
+    "../codemirror/mode/javascript/javascript"
+  ], function(CodeMirror) {
+    CodeMirror.fromTextArea(myFunction, {
+      lineNumbers: true,
+      mode: "javascript",
+      theme: "night"
+    });
+  });
 };
-
-document.getElementById("button1").addEventListener("click", function() {
-    var height = canvas.offsetHeight;
-    var width = canvas.offsetWidth;
-    var growth = height + 50;
-    var large = width + 50;
-    canvas.style.width = large + 'px';
-    canvas.style.height = growth + 'px';
-});
-
-document.getElementById("button2").addEventListener("click", function() {
-	canvas.style.opacity = 1;
-  	ctx.fillStyle = "blue";
-  	ctx.fillRect(0, 0, cw, ch);
-});
-
-document.getElementById("button3").addEventListener("click", function() {
-	canvas.style.opacity = 0.3;
-});
-
-document.getElementById("button4").addEventListener("click", function() {
-	canvas.style.opacity = 1;
-	var grd = ctx.createLinearGradient(0, 0, 0, ch);
-		grd.addColorStop(0, "red");
-		grd.addColorStop(.18, "orange");
-		grd.addColorStop(.36, "yellow");
-		grd.addColorStop(.54, "green");
-		grd.addColorStop(.69, "blue");
-		grd.addColorStop(.87, "indigo");
-		grd.addColorStop(1, "violet");
-	
-	ctx.fillStyle = grd;
-	ctx.fillRect(0, 0, cw, ch);
-
-});
-
-document.getElementById("button5").addEventListener("click", function() {
-	canvas.style.opacity = 1;
-	ctx.fillStyle = "black"
-	ctx.font = "50px Arial black";
-	ctx.fillText("Wonderful!",10,50);
-
-});	
-
-document.getElementById("button6").addEventListener("click", function() {
-  	canvas.reset = location.reload();
-});
