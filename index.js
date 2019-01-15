74d1df02ecefac0df8a4eb6c4d85033b61109c57
xezian <xezian@icloud.com>
2019-01-14 20:08:12 -0700

    I'm just thinking about this more and tidying it up superficially

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const assembleAndTestFunction = require("./methods/test-function");
+const testFunction = require("./methods/test-function").testFunction;
+const refreshFile = require("./methods/test-function").refreshFile;
 const app = express();
 const port = 3000;
 
@@ -10,11 +11,15 @@ const staticPath = path.join(__dirname, "/public");
 app.use(express.static(staticPath));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("index");
+app.get("/*", (req, res) => {
+  refreshFile().then(()=>{
+    res.redirect("index.html");
+  }).catch(err=>{
+    console.log(err);
+  })
 });
 app.post("/api/testfunction", (req, res) => {
-  assembleAndTestFunction(req.body)
+  testFunction(req.body)
     .then(results => {
       res.send(results);
     })
